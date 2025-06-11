8847bdd8d4ffe4698e5f54407ce6e3d46ddd50f1
gabemods <gabriel.longshaw@gmail.com>
2025-06-11 20:55:01 +0100

    Update main.js

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -1,7 +1,15 @@
 const menuToggle = document.getElementById('menu-toggle');
 const sideMenu = document.getElementById('side-menu');
+const overlay = document.getElementById('overlay');
 
 menuToggle.addEventListener('click', () => {
   menuToggle.classList.toggle('open');
   sideMenu.classList.toggle('open');
+  overlay.classList.toggle('show');
+});
+
+overlay.addEventListener('click', () => {
+  menuToggle.classList.remove('open');
+  sideMenu.classList.remove('open');
+  overlay.classList.remove('show');
 });
